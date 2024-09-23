72fe5d01cb2809e1286dbaa3cee501f8cb6a8158
SilentWraith2678 <parthsdeshpande@gmail.com>
2024-09-23 23:44:06 +0530

    Input Fields + SyncContactInfo func()

--- js/purchase-ticket.js
+++ js/purchase-ticket.js
@@ -1,2 +1,29 @@
-// 17:18
+//Yet to fix
+const sameContactCheckbox = document.getElementById('same-contact-checkbox');
+function syncContactInfo() {
+    if (sameContactCheckbox.checked) {
+    const firstNameInput = document.getElementById('first-name');
+    const lastNameInput = document.getElementById('last-name');
+    const emailInput = document.getElementById('email');
+    const phoneNumberInput = document.getElementById('phone-number');
 
+    const firstNameValue = firstNameInput.value;
+    const lastNameValue = lastNameInput.value;
+    const emailValue = emailInput.value;
+    const phoneNumberValue = phoneNumberInput.value;
+
+    // Insert the values into the other textboxes of the same classes
+    document.querySelectorAll('.first-name input').forEach((input) => {
+    input.value = firstNameValue;
+    });
+    document.querySelectorAll('.last-name input').forEach((input) => {
+    input.value = lastNameInput.value;
+    });
+    document.querySelectorAll('.email input').forEach((input) => {
+    input.value = emailValue;
+    });
+    document.querySelectorAll('.phone-number input').forEach((input) => {
+    input.value = phoneNumberValue;
+    });
+}
+}
