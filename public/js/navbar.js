6330f81103e0f8caf83c9559cd8dce4e6aec4812
BeanZ <92383674+BeanZ141@users.noreply.github.com>
2024-10-11 15:43:40 +0530

    Modified minor tweaks 15:43

--- public/js/navbar.js
+++ public/js/navbar.js
@@ -10,32 +10,22 @@ navLinks.forEach(link => {
 });
 
 // Toggle Theme
-const toggleButton = document.querySelector('.toggle-theme');
-const currentTheme = localStorage.getItem('theme') || 'dark'; 
+let darkmode = localStorage.getItem('dark-mode')
+const themeSwitch = document.getElementById('theme-switch')
 
-document.documentElement.setAttribute('data-theme', currentTheme);
-
-updateThemeIcon(currentTheme);
-
-toggleButton.addEventListener('click', () => {
-    let theme = document.documentElement.getAttribute('data-theme');
-    theme = (theme === 'light') ? 'dark' : 'light';
-
-    document.documentElement.setAttribute('data-theme', theme); 
-    localStorage.setItem('theme', theme);
-    updateThemeIcon(theme);
-});
-
-function updateThemeIcon(theme) {
-    const sunIcon = document.getElementById('light');
-    const moonIcon = document.getElementById('dark');
-    
-    if (theme === 'dark') {
-        sunIcon.style.display = 'block';
-        moonIcon.style.display = 'none';
-    } else {
-        sunIcon.style.display = 'none';
-        moonIcon.style.display = 'block';
-    }
+const enableDarkMode = () => {
+  document.body.classList.add('dark-mode')
+  localStorage.setItem('dark-mode', 'active')
 }
 
+const disableDarkMode = () => {
+  document.body.classList.remove('dark-mode')
+  localStorage.setItem('dark-mode', null)
+}
+
+if(darkmode === 'active') enableDarkMode()
+
+themeSwitch.addEventListener("click", () => {
+  darkmode = localStorage.getItem('dark-mode')
+  darkmode !== "active" ? enableDarkMode() : disableDarkMode()
+})
