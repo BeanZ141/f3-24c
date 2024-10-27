81c49bae6fca87a15a7ee4d750cd5dcfed66c322
BeanZ <92383674+BeanZ141@users.noreply.github.com>
2024-10-27 11:43:35 +0530

    Apply .gitignore rules 11:43

--- supabase.js
+++
@@ -1,3 +0,0 @@
-const supabaseUrl = 'https://xiwdkytqnabqawssehrg.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inhpd2RreXRxbmFicWF3c3NlaHJnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjY5NDE0NTEsImV4cCI6MjA0MjUxNzQ1MX0.4r_O1Za9Q41zpHxdx0JuloECBa-bw7e4m93v241rpgw';
-const supabase = window.supabase.createClient(supabaseUrl, supabaseKey);
